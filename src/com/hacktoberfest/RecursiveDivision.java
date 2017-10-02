3a906f1597079cb496a10ed632b33f92777922b9
Alexandre Insua <alexandreinsua@gmail.com>
2017-10-02 21:19:13 +0200

    I Added a recursive division.

---
+++ src/com/hacktoberfest/RecursiveDivision.java
@@ -0,0 +1,22 @@
+package com.hacktoberfest;
+
+public class RecursiveDivision {
+	public static void main(String[] args) {
+
+		System.out.println(division(20, 2));
+
+	}
+
+	public static int division(int a, int b) {
+		if (b > a)
+			// It exits when the numerator (a) is less than the denominator (b).
+			return 0;
+		else
+			// When numerator him rest on the denominator.
+			// Each time you do this consciously or increased by 1.
+			// This 1 acts as a constant that is adding on
+			
+			return division(a - b, b) + 1;
+	}
+
+}
